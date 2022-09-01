0ccf599782e87dada6899ba99b0ec12929b2699a
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-09-01 14:56:08 +0200

    Initial commit

---
+++ apps/lazy-loading-modules-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-test-apps-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'lazy-loading-modules-app';
+}
